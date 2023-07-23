e3a8152d6642f026c84d153da68cd468ed55e060
ethanp <ethanp.dev@gmail.com>
2023-07-23 14:15:26 +0700

    feat: CRUD Order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      database: 'micro_order',
+      username: 'ethanp',
+      password: '%sJgsgkTtn3dS2',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { protobufPackage } from './order/proto/product.pb';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50052',
+      package: protobufPackage,
+      protoPath: join('node_modules/grpc-nest-proto/proto/order.proto'),
+    },
+  });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.listen();
 }
 bootstrap();
